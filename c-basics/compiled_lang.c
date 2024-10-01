f6af9cabc1ba7a89fde68965bdd10fd9450ed9ea
Raushan Kumar <raushan.development@gmail.com>
2024-10-01 13:43:52 +0530

    solve: compile issue

--- c-basics/compiled_lang.c
+++ c-basics/compiled_lang.c
@@ -2,12 +2,12 @@
 
 int main(){
     printf("We will check that C is a compiled language. \n ");
-    does_not_exists("failure"); 
-    printf("Would never run. \n");
+    // does_not_exists("failure"); 
+    printf("It ran because non existing function call was removed. \n");
 
     return 0;
 }
-// the program will not compile: 
+// the program will not compile if does_not_exists("failure") is used: 
 // error: call to undeclared function 'does_not_exists'; ISO C99 and later do not support implicit function declarations [-Wimplicit-function-declaration]
 //     does_not_exists("failure");
 //     ^
