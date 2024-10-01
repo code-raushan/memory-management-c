8a7616f57e744a3d1bf5992f59e2d38b1e92d701
Raushan Kumar <raushan.development@gmail.com>
2024-10-01 16:25:33 +0530

    add: struct returning function

--- structs/coords.c
+++ structs/coords.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include "coords.h"
 
+
+struct Coordinate print_coord(int x, int y, int z ) {
+    struct Coordinate c_print = {
+        .x = x,
+        .y = y,
+        .z = z
+    };
+
+    printf("x: %d, y: %d, z: %d \n", c_print.x, c_print.y, c_print.z);
+
+    return c_print;
+}
+
 int main(){
     struct Coordinate c = {0}; // zero initializer
     printf("x: %d, y: %d, z: %d \n", c.x, c.y, c.z);
@@ -15,6 +28,8 @@ int main(){
     }; // designated initializer
 
     printf("x: %d, y: %d, z: %d \n", c_des.x, c_des.y, c_des.z);
+
+    print_coord(10, 20, 30);
 
     return 0;
 }
