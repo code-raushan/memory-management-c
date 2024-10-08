189e5ff4b16ccf7f69417e8bce9f24b7205dee16
Raushan Kumar <raushan.development@gmail.com>
2024-10-08 17:58:58 +0530

    add: comments about pointers

--- pointers/pointer.c
+++ pointers/pointer.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * Pointers are variables that store the memory address of another variable.
+ * They are declared using the * symbol.
+ * 
+ * & is the address-of operator, which gives the memory address of a variable (returns the memory address of the variable).
+ * * is the pointer operator, which gives the value of the variable stored in the pointer (dereferencing the pointer).
+ */
+
 int main(){
     int meaning_of_life = 789;
     int *pointer_to_meaning_of_life = &meaning_of_life;
