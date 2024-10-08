55df4f7bb8d77d30a37cc2af2d3a35f2a0efdc12
Raushan Kumar <raushan.development@gmail.com>
2024-10-08 18:21:20 +0530

    add: arrays definition

---
+++ arrays/array.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int arr[3] = {1, 2, 3};
+
+    printf("arr[0] = %d\n", arr[0]);
+    printf("arr[1] = %d\n", arr[1]);
+    printf("arr[2] = %d\n", arr[2]);
+
+    return 0;
+}
